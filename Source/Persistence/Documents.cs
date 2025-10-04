de30fb5f45b690e8bf9231da61f889ead895d302
Richard Garcia <code.richardy@gmail.com>
2025-10-03 23:27:02 -0300

    this commit introduces the documents class

---
+++ Source/Persistence/Documents.cs
@@ -0,0 +1,12 @@
+namespace Vinder.Internal.Infrastructure.Persistence;
+
+internal static class Documents
+{
+    public static class Activity
+    {
+        public const string Identifier = "_id";
+        public const string Action = "Action";
+        public const string UserId = "UserId";
+        public const string TenantId = "TenantId";
+    }
+}
