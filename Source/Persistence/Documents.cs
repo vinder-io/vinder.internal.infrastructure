046f80b19fb4d48b53b5756a1e9f87a1a19d4f21
Richard Garcia <code.richardy@gmail.com>
2025-10-04 20:39:05 -0300

    this commit introduces new document field definitions
    this commit introduces new document field definitions for resource, user and tenant identifiers.

--- Source/Persistence/Documents.cs
+++ Source/Persistence/Documents.cs
@@ -6,7 +6,8 @@ internal static class Documents
     {
         public const string Identifier = "_id";
         public const string Action = "Action";
-        public const string UserId = "UserId";
-        public const string TenantId = "TenantId";
+        public const string Resource = "Resource.Identifier";
+        public const string UserId = "User._id";
+        public const string TenantId = "Tenant._id";
     }
 }
