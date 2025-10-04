c3a85234816b569dd3b2f7eaf3fc2fd08e28905c
Richard Garcia <code.richardy@gmail.com>
2025-10-03 23:27:42 -0300

    this commit introduces the activity repository

---
+++ Source/Persistence/Repositories/ActivityRepository.cs
@@ -0,0 +1,41 @@
+namespace Vinder.Internal.Infrastructure.Persistence.Repositories;
+
+public sealed class ActivityRepository(IMongoDatabase database) :
+    BaseRepository<Activity>(database, Collections.Activities),
+    IActivityRepository
+{
+    public async Task<IReadOnlyCollection<Activity>> GetActivitiesAsync(
+        ActivityFilters filters, CancellationToken cancellation = default)
+    {
+        var pipeline = PipelineDefinitionBuilder
+            .For<Activity>()
+            .As<Activity, Activity, BsonDocument>()
+            .FilterActivities(filters)
+            .Paginate(filters.Pagination)
+            .Sort(filters.Sort);
+
+        var options = new AggregateOptions { AllowDiskUse = true };
+        var aggregation = await _collection.AggregateAsync(pipeline, options, cancellation);
+
+        var bsonDocuments = await aggregation.ToListAsync(cancellation);
+        var activities = bsonDocuments
+            .Select(bson => BsonSerializer.Deserialize<Activity>(bson))
+            .ToList();
+
+        return activities;
+    }
+
+    public async Task<long> CountAsync(ActivityFilters filters, CancellationToken cancellation = default)
+    {
+        var pipeline = PipelineDefinitionBuilder
+            .For<Activity>()
+            .As<Activity, Activity, BsonDocument>()
+            .FilterActivities(filters)
+            .Count();
+
+        var aggregation = await _collection.AggregateAsync(pipeline, cancellationToken: cancellation);
+        var result = await aggregation.FirstOrDefaultAsync(cancellation);
+
+        return result?.Count ?? 0;
+    }
+}
