32686c744a3ac5d2eeede8b2104e9e8ca9693116
Richard Garcia <code.richardy@gmail.com>
2025-10-03 23:26:25 -0300

    this commit introduces a null check for pagination filters

--- Source/Persistence/Pipelines/PaginationFiltersStage.cs
+++ Source/Persistence/Pipelines/PaginationFiltersStage.cs
@@ -4,8 +4,11 @@ public static class PageFiltersStage
 {
     public static PipelineDefinition<TInput, TOutput> Paginate<TInput, TOutput>(
         this PipelineDefinition<TInput, TOutput> pipeline,
-        PaginationFilters filters)
+        PaginationFilters? filters)
     {
+        if (filters is null)
+            return pipeline;
+
         return pipeline
             .Skip(filters.Skip)
             .Limit(filters.Take);
