4e7ba109dfe06b66970b9224631445952489636b
Richard Garcia <code.richardy@gmail.com>
2025-10-03 21:57:36 -0300

    this commit introduces pagination filters stage

---
+++ Source/Persistence/Pipelines/PaginationFiltersStage.cs
@@ -0,0 +1,13 @@
+namespace Vinder.Internal.Infrastructure.Persistence.Pipelines;
+
+public static class PageFiltersStage
+{
+    public static PipelineDefinition<TInput, TOutput> Paginate<TInput, TOutput>(
+        this PipelineDefinition<TInput, TOutput> pipeline,
+        PaginationFilters filters)
+    {
+        return pipeline
+            .Skip(filters.Skip)
+            .Limit(filters.Take);
+    }
+}
