ec375d66ff9caa639c95728501e88499da35adfb
Richard Garcia <code.richardy@gmail.com>
2025-10-03 23:27:09 -0300

    this commit introduces the activity filters stage

---
+++ Source/Persistence/Pipelines/ActivityFiltersStage.cs
@@ -0,0 +1,19 @@
+namespace Vinder.Internal.Infrastructure.Persistence.Pipelines;
+
+public static class ActivityFiltersStage
+{
+    public static PipelineDefinition<Activity, BsonDocument> FilterActivities(
+        this PipelineDefinition<Activity, BsonDocument> pipeline,
+        ActivityFilters filters)
+    {
+        var definitions = new List<FilterDefinition<BsonDocument>>
+        {
+            FilterDefinitions.MatchIfNotEmpty(Documents.Activity.Identifier, filters.Id),
+            FilterDefinitions.MatchIfNotEmpty(Documents.Activity.Action, filters.Action),
+            FilterDefinitions.MatchIfNotEmpty(Documents.Activity.TenantId, filters.TenantId),
+            FilterDefinitions.MatchBool(Documents.Activity.UserId, filters.IsDeleted)
+        };
+
+        return pipeline.Match(Builders<BsonDocument>.Filter.And(definitions));
+    }
+}
