20d7327d70774039d5f8cf30a66bfcaef2a50bdf
Richard Garcia <code.richardy@gmail.com>
2025-10-03 21:57:13 -0300

    this commit introduces global usings

---
+++ Source/Usings.cs
@@ -0,0 +1,11 @@
+/* global usings for vinder namespaces here */
+
+global using Vinder.Internal.Essentials.Filters;
+global using Vinder.Internal.Essentials.Contracts;
+global using Vinder.Internal.Essentials.Entities;
+global using Vinder.Internal.Essentials.Utilities;
+
+/* global usings for others namespaces here */
+
+global using MongoDB.Driver;
+global using MongoDB.Bson;
